81d5f751b207cebb7f3158faf209410ee40bc7c7
Sankarshan A Joshi <sankarshan.joshi@neutrinos.co>
2019-09-23 00:32:30 +0530

    Use node's "experimental-modules" featue. I mean, why not!

--- test/debug.js
+++ test/debug.js
@@ -1,6 +1,5 @@
-const utils = require('../lib/utilFuncs');
-
-
+// const utils = require('../lib/utilFuncs');
+import utils from '../lib/utilFuncs.js';
 const o = {
     a: "lfkglkfg",
     b: "pppppppp",
@@ -27,7 +26,9 @@ const o = {
     lk: "asdasd",
 };
 
-let result;
-result = utils.deepReplace(o, 'a', 'MothahFkinA', 'all');
-result = utils.deepSearch(o, 'a');
-console.log(result);
+let result; // because, hovering >>> typing.
+result = utils.deepReplace(o, 'a', 'MothahFkinA', 'all'); console.log(result);
+result = utils.deepSearch(o, 'l'); console.log(result);
+result = utils.getURLQParams('http://sdf.sdfs.com/llk?bagOf=fur&suma=cumlaude'); console.log(result);
+result = utils.kebabFromPorC('YouCanPrepareKebabFromPork'); console.log(result);
+result = utils.pOrCFromKebab('you-can-get-pork-from-kebab-too'); console.log(result);
