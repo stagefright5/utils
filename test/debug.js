e08ddbe979285095af961c59536a9e15eee20f1e
stagefright5 <sankarshanaj@gmail.com>
2020-01-01 20:12:42 +0530

    format and cleanup

--- test/debug.js
+++ test/debug.js
@@ -1,35 +1,70 @@
 // const utils = require('../lib/utilFuncs');
 import utils from '../lib/utilFuncs.js';
 const o = {
-    qwerty: "lfkglkfg",
-    b: "pppppppp",
+    qwerty: 'lfkglkfg',
+    b: 'pppppppp',
     f: {
-        dnc: "ssdsdf",
+        dnc: 'ssdsdf',
         qw: {
             sgdhf: 999,
             f: {
-                mnb: "weah",
-                a: "wewdef"
+                mnb: 'weah',
+                a: 'wewdef',
             },
-            a: "asAS"
+            a: 'asAS',
         },
-        a: "ssdsdf"
+        a: 'ssdsdf',
     },
     s: {
         l: {
-            g: "sfdd", trt: "dasfdf",
-            a: 'edrtert'
+            g: 'sfdd', trt: 'dasfdf',
+            a: 'edrtert',
         },
-        a: 'sdasd'
+        a: 'sdasd',
     },
-    c: "ppppopopopopo",
-    lk: "asdasd",
+    c: 'ppppopopopopo',
+    lk: 'asdasd',
+};
+
+const o2 = {
+    a: [
+        'asdas',
+        89,
+    ],
+    l: {
+        d: {
+            f: 0,
+            k: [
+                1,
+                23,
+                88,
+            ],
+        },
+        oppp: 'asdasd',
+    },
+    uuu: {
+        lll: [],
+        },
+    weq: {
+        d: 'sadasd',
+    },
 };
 
 let result; // because, hovering >>> typing.
-result = utils.deepReplace(o, 'a', 'MothahFkinA'); console.log(result);
-result = utils.deepReplace(o, 'a', 'MothahFkinA', 'all'); console.log(result);
-result = utils.deepSearch(o, 'l'); console.log(result);
-result = utils.getURLQParams('http://sdf.sdfs.com/llk?bagOf=fur&suma=cumlaude'); console.log(result);
-result = utils.kebabFromPorC('YouCanPrepareKebabFromPork'); console.log(result);
-result = utils.pOrCFromKebab('you-can-get-pork-from-kebab-too'); console.log(result);
+result = utils.deepReplace(o, 'a', 'MothahFkinA');
+console.log(result);
+
+result = utils.deepReplace(o, 'a', 'MothahFkinA', 'all');
+console.log(result);
+
+result = utils.deepSearch(o, 'l');
+console.log(result);
+
+result = utils.getURLQParams('http://sdf.sdfs.com/llk?bagOf=fur&suma=cumlaude');
+console.log(result);
+
+result = utils.kebabFromPorC('YouCanPrepareKebabFromPork');
+console.log(result);
+
+result = utils.pOrCFromKebab('you-can-get-pork-from-kebab-too');
+console.log(result);
